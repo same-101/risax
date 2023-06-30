81898aa4cf027bca9c5c02edf905486674c3b5ab
same-101 <117522827+same-101@users.noreply.github.com>
2023-06-29 20:50:24 -0400

    Update risax.js

--- risax.js
+++ risax.js
@@ -2,7 +2,7 @@ const title = document.title;
 const icon = document.querySelector('link[rel="icon"]');
 
 document.title = "Risax App";
-icon.href = "./icon.png";
+icon.href = "https://raw.githubusercontent.com/same-101/risax/main/icon.png";
 
 function Return (HTML) {
     document.body.innerHTML += HTML;
